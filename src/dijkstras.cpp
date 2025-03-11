968b2451410b6d1e5cf67caad963dcec43700074
Aanya Agrawal <aanyaa2@uci.edu>
2025-03-11 06:29:02 +0000

    Fxed printing stuff

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -41,12 +41,12 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "" << endl;
+        cout << "\nTotal cost is " << total << endl;
         return;
     }
     for (size_t i = 0; i < path.size(); ++i) {
-        cout << path[i];
-        if (i < path.size() - 1) cout << " ";
+        cout << path[i]<< " ";
+        // if (i < path.size() - 1) cout << " ";
     }
     cout << "\nTotal cost is " << total << endl;
 }
